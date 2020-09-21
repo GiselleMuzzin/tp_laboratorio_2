62e495ea4f4dddc33b9532ddeb11954b0fb91295
gise <muzzingiselle@gmail.com>
2020-09-21 19:19:21 -0300

    Estructura inicial del tp

---
+++ ArrayEmployees.h
@@ -0,0 +1,18 @@
+#ifndef ARRAYEMPLOYEES_H_INCLUDED
+#define ARRAYEMPLOYEES_H_INCLUDED
+
+#include "employee.h"
+
+int initEmployees(Employee* list, int len);
+
+int addEmployee(Employee* list, int len, int id, char name[],char lastName[],float salary,int sector);
+
+int findEmployeeById(Employee* list, int len,int id);
+
+int removeEmployee(Employee* list, int len, int id);
+
+int sortEmployees(Employee* list, int len, int order);
+
+int printEmployees(Employee* list, int length);
+
+#endif // ARRAYEMPLOYEES_H_INCLUDED
